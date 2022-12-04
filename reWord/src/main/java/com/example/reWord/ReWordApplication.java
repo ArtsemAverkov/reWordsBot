5afc24f5f9a8ecc7502e4ab42cdf82244086546a
ArtemAverkov <temaaak@mail.ru>
2022-12-04 15:33:11 +0200

    first commit

---
+++ reWord/src/main/java/com/example/reWord/ReWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.reWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReWordApplication.class, args);
+	}
+
+}
---
+++ reWord/src/test/java/com/example/reWord/ReWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
